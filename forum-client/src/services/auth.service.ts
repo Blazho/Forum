10ad4cd7a3cb5c1686ce4e4e0c33de864cc1f1aa
Aleksandar Blazhevski <aleksandar.blazhevski@outlook.com>
2025-03-19 16:08:13 +0100

    Log in functionality

--- forum-client/src/services/auth.service.ts
+++ forum-client/src/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { RegisterRequest } from "../api-interfaces/requests/register.request";
+import { LogInRequest } from "../api-interfaces/requests/login.request";
 
 @Injectable({
     providedIn: 'root'
@@ -10,8 +11,8 @@ export class AuthService{
     private path = '/api/auth'
     private http = inject(HttpClient)
 
-    login(username: string, password: string): any{
-        //todo
+    login(logInRequest: LogInRequest): Observable<string>{
+        return this.http.post<string>(`${this.path}/login`, logInRequest);
     }
 
     register(registerObject: RegisterRequest) : Observable<string> {
