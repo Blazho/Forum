297229af8004815571c1ec321e31079ff35213ec
Aleksandar Blazhevski <aleksandar.blazhevski@outlook.com>
2025-03-19 20:27:21 +0100

    Nav buttons and width bug

--- forum-client/src/components/reusables/navigation-menu/navigation-menu.component.ts
+++ forum-client/src/components/reusables/navigation-menu/navigation-menu.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'navigation-menu',
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatButtonModule],
   templateUrl: './navigation-menu.component.html',
   styleUrl: './navigation-menu.component.css'
 })
